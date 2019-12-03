cebe7df2992ee816b83113b99fc968b948f28fcd
Nick <nick-1996@yandex.ru>
2019-12-03 20:50:58 +0300

    useless fixes

--- app/src/main/java/ru/skillbranch/gameofthrones/AppActivity.kt
+++ app/src/main/java/ru/skillbranch/gameofthrones/AppActivity.kt
@@ -3,13 +3,12 @@ package ru.skillbranch.gameofthrones
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import ru.skillbranch.gameofthrones.ui.splash.SplashFragment
-import ru.skillbranch.gameofthrones.R
 
 class AppActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
-        supportFragmentManager.beginTransaction().add(R.id.root_content, SplashFragment()).commit()
+        supportFragmentManager.beginTransaction().add(R.id.root_container, SplashFragment()).commit()
     }
 }
